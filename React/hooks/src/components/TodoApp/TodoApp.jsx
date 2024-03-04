7cbb0b901b53ed975bec0e8720ebbb92069cc6ba
Pablo <pablovicente19@hotmail.com>
2024-03-04 07:22:50 -0300

    termine la todo

--- React/hooks/src/components/TodoApp/TodoApp.jsx
+++ React/hooks/src/components/TodoApp/TodoApp.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import {
   ContainerStyled,
   ButtonStyled,
   FormStyled,
   InputStyled,
+  TasksContainerStyled,
+  Task,
 } from "./TodoAppStyles";
 
 const TodoApp = () => {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
+  const inputRef = useRef(null);
+
+  const addTask = () => {
+    setTask((prevTask) => [...prevTask, newTask]);
+    setNewTask("");
+  };
+  const removeTask = (index) => {
+    setTask((prevTasks) => {
+      const updateTasks = [...prevTasks];
+      updateTasks.splice(index, 1);
+      return updateTasks;
+    });
+  };
   return (
     <ContainerStyled>
-      <FormStyled>
-        <InputStyled></InputStyled>
-        <ButtonStyled>Agregar</ButtonStyled>
+      <FormStyled onSubmit={(e) => e.preventDefault(e)}>
+        <InputStyled
+          type="Text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          ref={inputRef}
+        />
+        <ButtonStyled onClick={addTask}>Agregar</ButtonStyled>
       </FormStyled>
+      <TasksContainerStyled>
+        {task.map((tasks) => {
+          return <Task>{tasks}</Task>;
+        })}
+      </TasksContainerStyled>
     </ContainerStyled>
   );
 };
